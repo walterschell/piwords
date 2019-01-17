3b634219a4c1a6cca54280048441070454d5a078
1BADragon <matthewbrown92@gmail.com>
2019-01-16 21:31:13 -0500

    working on main function

--- src/gen_val.c
+++ src/gen_val.c
@@ -5,9 +5,8 @@
 static int index = 1;
 static unsigned char buffer = 0;
 static size_t buffer_size = 0;
-const static unsigned char top_5 = 0xf8;
 
-static unsigned char get_mask();
+static unsigned char get_mask(size_t mask_size);
 
 int next_val(void)
 {
@@ -16,13 +15,13 @@ int next_val(void)
     buffer &= (temp & get_mask(buffer_size)) >> buffer_size;
 
     unsigned char ret = buffer & get_mask(5);
-    buffer = temp << (8 - buffer_size);
+    buffer = (temp << (8 - buffer_size)) & 0xff;
     buffer_size = (buffer_size + 5) % 8;
 
-    return ret;
+    return ret >> 3;
 }
 
 static unsigned char get_mask(size_t mask_size)
 {
-    return 0xff << (8 - mask_size);
+    return (0xff << (8 - mask_size)) & 0xff;
 }
--- src/piwords.cpp
+++ src/piwords.cpp
@@ -1,6 +1,36 @@
 #include <trie.h>
+#include <string>
+#include <list>
+#include <iostream>
 
-int main()
+#include "gen_val.h"
+
+int main(int argc, char ** argv)
 {
+    size_t num_digits;
+    char * file_name;
+    if(argc == 2)
+    {
+        num_digits = 10000;
+        file_name = argv[1];
+    }
+    else if (argc == 3)
+    {
+        num_digits = std::stoul(std::string(argv[1]));
+        file_name = argv[2];
+    }
+    else {
+        std::cerr << "Usage: [digits of pi] <file name>" << std::endl;
+        return 1;
+    }
+
+    std::list<int> digits;
+    while(num_digits-- > 0)
+    {
+        digits.push_back(next_val());
+    }
+
+
+
     return 0;
 }
