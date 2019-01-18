0ab15400f22251450d325836acb4dfb38706b962
1BADragon <matthewbrown92@gmail.com>
2019-01-17 21:48:39 -0500

    really slow

--- src/piwords.cpp
+++ src/piwords.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <list>
 #include <iostream>
+#include <fstream>
 
 #include "gen_val.h"
 
@@ -24,13 +25,42 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    std::list<int> digits;
+    std::vector<int> digits;
     while(num_digits-- > 0)
     {
         digits.push_back(next_val());
     }
 
+    std::ifstream inf(file_name);
 
+    if (!inf)
+    {
+        std::cerr << "Unable to open file" << std::endl;
+        return 1;
+    }
+
+    Trie trie;
+
+    while(inf)
+    {
+        string word;
+        inf >> word;
+        trie.add_word(word);
+    }
+    std::list<Match> matches;
+    std::list<TrieNode *> partial_matches;
+
+    match_sequence(digits, 0, matches, partial_matches,
+                   trie, 3);
+
+    std::cout << "Found " << matches.size() << " matches." << std::endl;
+
+    for(auto match : matches)
+    {
+        std::cout << match.index << ": " << match.word << std::endl;
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
